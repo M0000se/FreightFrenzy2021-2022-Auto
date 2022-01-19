6aa7d960e03018f5cf094bdd01642ce074760565
M0000se <51048817+M0000se@users.noreply.github.com>
2022-01-19 14:13:12 +0200

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,29 +11,32 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
-        MeepMeep meepMeep = new MeepMeep(800);
+        Pose2d startPose = new Pose2d(0, -60, Math.toRadians(90));
+        Vector2d location2 = new Vector2d(-12, -42);
+        Vector2d midpoint = new Vector2d(-30, -42);
+        Pose2d location3 = new Pose2d(-45, -55, Math.toRadians (0));
+        Vector2d storage = new Vector2d(56, -64);
+
+        MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .setConstraints(50, 50, 25, 25,
+                        12)
                 // Option: Set theme. Default = ColorSchemeRedDark()
                 .setColorScheme(new ColorSchemeRedDark())
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .addDisplacementMarker(() -> {
-                                    /* Everything in the marker callback should be commented out */
 
-                                    // bot.shooter.shoot()
-                                    // bot.wobbleArm.lower()
-                                })
-                                .turn(Math.toRadians(90))
-                                .splineTo(new Vector2d(10, 15), 0)
-                                .turn(Math.toRadians(90))
-                                .build()
-                );
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose)
+                                .lineTo(location2)//location of the red shipping hub
+                                .addTemporalMarker(1000, () -> {}) //servo dump
+                                .waitSeconds(1)
+                                .lineToLinearHeading(location3)
+                                .addTemporalMarker(5000, () -> {}) // motor spinner wheel
+                                .waitSeconds(6)
+                                .splineTo((storage), Math.toRadians(90))
+                                .build());
+
 
         // Set field image
         meepMeep.setBackground(MeepMeep.Background.FIELD_FREIGHTFRENZY_ADI_DARK)
