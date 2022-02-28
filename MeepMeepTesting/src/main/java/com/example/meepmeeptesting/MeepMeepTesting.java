69d7bdb2b798fc7e4d471a53851802c73f99eff4
M0000se <51048817+M0000se@users.noreply.github.com>
2022-02-28 03:13:55 -0400

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -12,14 +12,45 @@ public class MeepMeepTesting {
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
 
-
-
-        Pose2d startPose = new Pose2d(0, -60, Math.toRadians(90));
-        Vector2d location2 = new Vector2d(-12, -42);
+        Pose2d startPose = new Pose2d(0, 60, Math.toRadians(-90));
+        Vector2d location2 = new Vector2d(-12, 42);
         //Vector2d midpoint = new Vector2d(-30, -42);
-        Pose2d spinner = new Pose2d(-45, -60, Math.toRadians (90));
-        Pose2d warehouse_midpoint = new Pose2d(-40, -65, Math.toRadians (0));
-        Pose2d warehouse = new Pose2d(40, -65, Math.toRadians (0));
+        Pose2d spinner = new Pose2d(-60, 45, Math.toRadians (0));
+        Pose2d spinner_shift = new Pose2d(-60, 50, Math.toRadians (0));
+        //Pose2d spinner_shift = new Pose2d(-55, 55, Math.toRadians (0));
+        Vector2d warehouse_midpoint = new Vector2d(-25, 65);
+        Pose2d warehouse = new Pose2d(40, 65, Math.toRadians (0));
+
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, 25, 25,
+                        12)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose)
+                                .lineTo(location2)//location of the red shipping hub
+                                .addTemporalMarker(1000, () -> {}) //servo dump
+                                .waitSeconds(1)
+                                .lineToLinearHeading(spinner)
+                                .lineToLinearHeading(spinner_shift)
+                                .addTemporalMarker(5000, () -> {}) // motor spinner wheel
+                                .waitSeconds(3.5)
+                                .splineToConstantHeading(warehouse_midpoint, 0)
+                                .lineToLinearHeading(warehouse)
+                                .build());
+
+
+        /*Pose2d startPose = new Pose2d(0, 60, Math.toRadians(-90));
+        Vector2d location2 = new Vector2d(-12, 42);
+        //Vector2d midpoint = new Vector2d(-30, -42);
+        Pose2d spinner = new Pose2d(-50, 55, Math.toRadians (90));
+        //Pose2d spinner_shift = new Pose2d(-55, 55, Math.toRadians (0));
+        Pose2d warehouse_midpoint = new Pose2d(-20, 60, Math.toRadians (0));
+        Pose2d warehouse = new Pose2d(40, 65, Math.toRadians (0));
 
         MeepMeep meepMeep = new MeepMeep(700);
 
@@ -37,11 +68,11 @@ public class MeepMeepTesting {
                                 .waitSeconds(1)
                                 .lineToLinearHeading(spinner)
                                 .addTemporalMarker(5000, () -> {}) // motor spinner wheel
-                                .waitSeconds(6)
+                                .waitSeconds(3.5)
                                 .lineToLinearHeading(warehouse_midpoint)
                                 .lineToLinearHeading(warehouse)
                                 .build());
-
+        */
 
 
         /*Pose2d startPose = new Pose2d(0, -60, Math.toRadians(90));
@@ -67,7 +98,7 @@ public class MeepMeepTesting {
                                 .waitSeconds(1)
                                 .lineToLinearHeading(spinner)
                                 .addTemporalMarker(5000, () -> {}) // motor spinner wheel
-                                .waitSeconds(6)
+                                .waitSeconds(3.5)
                                 .lineToLinearHeading(warehouse_midpoint)
                                 .lineToLinearHeading(warehouse)
                                 .build());
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Global_settings.java
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode;
+
+public class Global_settings
+{
+    //test trajectory blue 1 variables
+    static int
+}
