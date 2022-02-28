9d38e69494a3ec58c026e790f18ef2ad9728410e
M0000se <51048817+M0000se@users.noreply.github.com>
2022-02-27 20:59:07 -0400

    Update MeepMeepTesting.java

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -11,6 +11,9 @@ public class MeepMeepTesting {
     public static void main(String[] args) {
         // Declare a MeepMeep instance
         // With a field size of 800 pixels
+
+
+
         Pose2d startPose = new Pose2d(0, -60, Math.toRadians(90));
         Vector2d location2 = new Vector2d(-12, -42);
         //Vector2d midpoint = new Vector2d(-30, -42);
@@ -38,6 +41,38 @@ public class MeepMeepTesting {
                                 .lineToLinearHeading(warehouse_midpoint)
                                 .lineToLinearHeading(warehouse)
                                 .build());
+
+
+
+        /*Pose2d startPose = new Pose2d(0, -60, Math.toRadians(90));
+        Vector2d location2 = new Vector2d(-12, -42);
+        //Vector2d midpoint = new Vector2d(-30, -42);
+        Pose2d spinner = new Pose2d(-45, -60, Math.toRadians (90));
+        Pose2d warehouse_midpoint = new Pose2d(-40, -65, Math.toRadians (0));
+        Pose2d warehouse = new Pose2d(40, -65, Math.toRadians (0));
+
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Required: Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, 25, 25,
+                        12)
+                // Option: Set theme. Default = ColorSchemeRedDark()
+                .setColorScheme(new ColorSchemeRedDark())
+
+                .followTrajectorySequence(drive ->
+                        drive.trajectorySequenceBuilder(startPose)
+                                .lineTo(location2)//location of the red shipping hub
+                                .addTemporalMarker(1000, () -> {}) //servo dump
+                                .waitSeconds(1)
+                                .lineToLinearHeading(spinner)
+                                .addTemporalMarker(5000, () -> {}) // motor spinner wheel
+                                .waitSeconds(6)
+                                .lineToLinearHeading(warehouse_midpoint)
+                                .lineToLinearHeading(warehouse)
+                                .build());
+
+         */
 
 
         // Set field image
