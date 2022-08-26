5bcf6c983811223bff8a652d14a79a84e010af2d
M0000se <51048817+M0000se@users.noreply.github.com>
2022-08-25 21:31:58 -0400

    -

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SubsystemConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SubsystemConstants.java
@@ -6,7 +6,7 @@ public final class SubsystemConstants
     public static final double color_error = 90; // max allowed color sensor rgb error
 
     public static final int x_center = 200; //middle of the webcam view
-    private static final int center_accuracy = 70;
+    public static final int center_accuracy = 70;
 
     public static int p_error = 50; // max allowed motor position error
     public static final double p = 0.001;
